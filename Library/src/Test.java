3d902a0cef2695f34bb05cac1896a7520ae09ae8
Tookie <gamingsiwel@gmail.com>
2018-11-02 14:41:38 +1030

    adding new project
    seperated by folder

--- Library/src/Test.java
+++ Library/src/Test.java
@@ -1,10 +1,9 @@
 import application.libraries.BookLibraryCode;
-import application.libraries.FilmLibraryCode;
 
 public class Test {
 
 	public static void main(String[] args) {
-		new FilmLibraryCode();
+		new BookLibraryCode();
 
 	}
 
